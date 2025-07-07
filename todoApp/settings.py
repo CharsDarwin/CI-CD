e4a9ad3513f056e1a714b568bff9929179f82136
CharsDarwin <darwin.chamaya@unas.edu.pe>
2025-07-07 09:26:26 -0500

    feat: add Procfile and gunicorn for Railway

--- todoApp/settings.py
+++ todoApp/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-
+ALLOWED_HOSTS = ["*", os.getenv("RAILWAY_STATIC_URL", "").replace("https://", "")]
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
